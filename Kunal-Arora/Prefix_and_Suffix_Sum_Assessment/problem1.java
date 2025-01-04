f61fac68742157f2e8c318ad6d973254ab8ab48d
arora-kunal <kunal.22SCSE1180152@galgotiasuniversity.edu.in>
2025-01-04 12:21:09 +0530

    Prefix and Suffix Sum Assessment

---
+++ Kunal-Arora/Prefix_and_Suffix_Sum_Assessment/problem1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class problem1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int n = sc.nextInt();
+        sc.close();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            
+        }
+        }
+    }
---
+++ Kunal-Arora/Prefix_and_Suffix_Sum_Assessment/problem2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class problem2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total number of flights:");
+        int n = sc.nextInt();
+        System.out.println("Enter range of flights and number of seats reserved for them:");
+        int bookings[] = new int[3];
+        sc.close();
+    }
+}
