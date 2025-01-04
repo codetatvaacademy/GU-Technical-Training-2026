9b896f20f3e3f737de4d5511f2a2b460e76a1b3a
Aniket-271 <aniketverma2603@gmail.com>
2025-01-04 12:21:14 +0530

    Create problemone.java

---
+++ AniketVerma/test1/problemone.java
@@ -0,0 +1,41 @@
+public class problem1{
+    
+    public static int fXn(int[]nums, int n,int middle_index,int leftmost,int rightmost){
+        
+        for (int i = 0; i < nums.length; i++){
+            if (nums[0]= middle_index[i]){
+            return 0;
+        }
+           else if (nums[n-1]= middle_index[i]){
+                return 0;
+            }
+            else if (middle_index[i]= nums[n-1]){
+                return int leftmost[] = int rightmost[] ;
+            }
+            else if( nums[0]= leftmost[i]){
+                return -1 ;
+            }
+        }
+
+
+
+    }
+    public static void main(String[] args) {
+        int [] nums =  new int[5];
+        nums[0] =2;
+        nums[1] =3;
+        nums[2] = -1;
+        nums[3] =8;
+        nums[4] =4;
+        fxn(nums,n, middle_index,leftmost,rightmost);
+
+
+
+
+
+    }
+}    
+
+    
+
+   
