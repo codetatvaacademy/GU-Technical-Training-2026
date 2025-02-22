30ed8ec183d3f029c8acb421e303c66e6d4c9f04
student-nirajkumar <neerajkumar89212@gmail.com>
2025-02-22 15:28:46 +0530

    Adding niraj folder

--- Antima_Dsa/ReadMe.md
+++
@@ -1,1 +0,0 @@
-Resume Link: https://drive.google.com/file/d/1P8EK8we6B-6PEH_MCHlB6n16sYF760tD/view?usp=sharing
---
+++ Nirajkumar22Scse1011423/BasicDSA/add.java
@@ -0,0 +1,7 @@
+class add{
+    public static void main(String[] args) {
+        int x=9;
+        int y=10;
+        System.out.print(x+y + " " );
+    }
+}
