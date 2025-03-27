5954dc2083f507e3abed038eb2d2340903c90565
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2025-03-27 22:35:26 +0530

    Added leapYear.java file

---
+++ AditiKaushik/BasicQuestions/leapYear.java
@@ -0,0 +1,25 @@
+// WAP to input year from user and check whether it is a leap year or not.
+
+import java.util.Scanner;
+
+public class leapYear {
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is NOT a Leap Year.");
+        }
+
+        scanner.close();
+    }
+}
+
+//Time Complexity: O(1)
+//Space Complexity: O(1)
