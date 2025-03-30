f77c06f2fd264f6bd052d0fe968e130505f3d111
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2025-03-30 20:34:48 +0530

    Added Questions

--- AditiKaushik/BasicQuestions/palindromeNumber.java
+++ AditiKaushik/BasicQuestions/palindromeNumber.java
@@ -1,24 +1,32 @@
-// WAP to print the reverse of a number given by user.
+// WAP to take input a number from the user and check whether it is palindrome.
 
 import java.util.Scanner;
 
-public class reverseNumber {
+public class palindromeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
         scanner.close();
-        System.out.println(reverse_Number(num));
+        
+        if (isPalindrome(num)) {
+            System.out.println("Palindrome.");
+        } else {
+            System.out.println("Not a palindrome.");
+        }
     }
 
-    public static int reverse_Number(int num) {
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
         int reverse = 0;
+        
         while (num != 0) {
             int digit = num % 10;
             reverse = reverse * 10 + digit;
             num /= 10;
         }
-        return reverse;
+        
+        return originalNum == reverse;
     }
 }
 
