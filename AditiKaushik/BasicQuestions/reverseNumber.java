ec80086c4ed480d555656d4a22b0866666138da0
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2025-03-30 20:31:08 +0530

    Added reverseNumber file

---
+++ AditiKaushik/BasicQuestions/reverseNumber.java
@@ -0,0 +1,23 @@
+// WAP to print the reverse of a number given by user.
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        System.out.println(reverse_Number(num));
+    }
+
+    public static int reverse_Number(int num) {
+        int reverse = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+        return reverse;
+    }
+}
